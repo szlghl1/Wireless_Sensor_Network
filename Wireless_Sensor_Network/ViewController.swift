9b2b2e541e20bff80ab35170e9300e17226e82f5
szlghl1 <szlghl1@163.com>
2016-12-07 01:54:24 -0600

    Initial Commit

---
+++ Wireless_Sensor_Network/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Wireless_Sensor_Network
+//
+//  Created by 凌何 on 16/12/7.
+//  Copyright © 2016年 凌何. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
